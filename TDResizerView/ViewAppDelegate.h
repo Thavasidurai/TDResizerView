4b49ee7f321add271d36d49ca4e08c7b0660fb07
Thavasidurai <thavasidurai@gmail.com>
2013-03-06 19:51:25 +0530

    Update ViewAppDelegate.h

--- TDResizerView/ViewAppDelegate.h
+++ TDResizerView/ViewAppDelegate.h
@@ -2,7 +2,7 @@
 //  ViewAppDelegate.h
 //  TDResizerView
 //
-//  Created by Purush V on 19/12/12.
+//  Created by Thavasidurai N on 19/12/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
